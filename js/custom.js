4c72edfd44974165b751f14e6cad7834bad8a3d6
Dave <dnbennett@hotmail.co.uk>
2016-09-12 12:51:09 +0100

    Code layout changes
    Code "beautified" in Atom

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,28 @@
 $(document).ready(function() {
 
-  var mapShown = false;
+    var mapShown = false;
 
-  $('#mapClick').on('click', function() {
-    if (!mapShown) {
-      $('.google-maps').removeClass('hidden');
-      $('#mapClose').removeClass('hidden');
-      mapShown = true;
-      var mapScroll = $("a[name='map']");
-      $('html, body').animate({scrollTop: mapScroll.offset().top}, 'slow');
+    $('#mapClick').on('click', function() {
+        if (!mapShown) {
+            $('.google-maps').removeClass('hidden');
+            $('#mapClose').removeClass('hidden');
+            mapShown = true;
+            var mapScroll = $("a[name='map']");
+            $('html, body').animate({
+                scrollTop: mapScroll.offset().top
+            }, 'slow');
+        };
+    });
+
+    $('#mapClose').on('click', function() {
+        if (mapShown) {
+            $('.google-maps').addClass('hidden');
+            $('#mapClose').addClass('hidden');
+            mapShown = false;
+        };
+    });
+
+    if (document.documentElement.clientWidth < 768) {
+        $('.logo').removeClass('fa-3x').addClass('fa-2x');
     };
-  });
-
-  $('#mapClose').on('click', function() {
-    if (mapShown) {
-      $('.google-maps').addClass('hidden');
-      $('#mapClose').addClass('hidden');
-      mapShown = false;
-    };
-  });
-
-  if (document.documentElement.clientWidth < 768) {
-    $('.logo').removeClass('fa-3x').addClass('fa-2x');
-  };
 });
