3e6fa45a0a2174c876cf5184f1ae78a06ea0dad3
Dave <dnbennett@hotmail.co.uk>
2016-09-14 13:23:17 +0100

    Added headline bar

--- js/custom.js
+++ js/custom.js
@@ -25,4 +25,26 @@ $(document).ready(function() {
     if (document.documentElement.clientWidth < 768) {
         $('.logo').removeClass('fa-3x').addClass('fa-2x');
     };
+
+    var headlines = ["Latest offer: Half price MOT & service for people called Dave", "News: Automatic Gearbox specialist now in house"];
+    var d = 1000; //delay time
+    var i = d * 6;
+
+    function showHead(headliner) {
+      var headline = $('#headline');
+      headline.text(headliner[0]).fadeIn(d);
+      headline.delay(d).fadeOut(d, function() {
+        $('#headline').text(headliner[1]);
+      }).fadeIn(d);
+      headline.delay(d).fadeOut(d);
+    };
+
+    showHead(headlines);
+
+
+    var textLoop = setInterval(function() {
+      showHead(headlines);
+    }, i);
+
+
 });
