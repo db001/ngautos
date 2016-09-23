d06c58190ed951bea04d58721d34d21981503d20
Dave <dnbennett@hotmail.co.uk>
2016-09-23 13:46:41 +0100

    Added more content

--- js/custom.js
+++ js/custom.js
@@ -1,49 +1,38 @@
 $(document).ready(function() {
 
-    var mapShown = false;
-
-    $('#mapClick').on('click', function() {
-        if (!mapShown) {
-            $('.google-maps').removeClass('hidden');
-            $('#mapClose').removeClass('hidden');
-            mapShown = true;
-            var mapScroll = $("a[name='map']");
-            $('html, body').animate({
-                scrollTop: mapScroll.offset().top
-            }, 'slow');
-        };
-    });
-
-    $('#mapClose').on('click', function() {
-        if (mapShown) {
-            $('.google-maps').addClass('hidden');
-            $('#mapClose').addClass('hidden');
-            mapShown = false;
-        };
+    $("#mapClick").on('click', function() {
+        var mapScroll = $("a[name='map']");
+        $('html, body').animate({
+            scrollTop: mapScroll.offset().top
+        }, 'slow');
     });
 
     if (document.documentElement.clientWidth < 768) {
         $('.logo').removeClass('fa-3x').addClass('fa-2x');
     };
 
-    var headlines = ["Latest offer: Half price MOT & service for people called Dave", "News: Automatic Gearbox specialist now in house"];
+    var headlines = [
+        "Latest offer: Half price MOT & service for people called Dave",
+        "News: Automatic Gearbox specialist now in house"
+    ];
+
     var d = 1000; //delay time
     var i = d * 6;
 
     function showHead(headliner) {
-      var headline = $('#headline');
-      headline.text(headliner[0]).fadeIn(d);
-      headline.delay(d).fadeOut(d, function() {
-        $('#headline').text(headliner[1]);
-      }).fadeIn(d);
-      headline.delay(d).fadeOut(d);
+        var headline = $('#headline');
+        headline.text(headliner[0]).fadeIn(d);
+        headline.delay(d).fadeOut(d, function() {
+            $('#headline').text(headliner[1]);
+        }).fadeIn(d);
+        headline.delay(d).fadeOut(d);
     };
 
     showHead(headlines);
 
 
     var textLoop = setInterval(function() {
-      showHead(headlines);
+        showHead(headlines);
     }, i);
 
 
