11dcf70f71f36ce7ce7703541f10a7c1e6ee0d4c
root <dnbennett@hotmail.co.uk>
2017-01-23 22:30:50 +0000

    Initial commit, refactoring code

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+
+    watch: {
+      sass: {
+        files: "app/scss/*.scss",
+        tasks: ['sass']
+      },
+      scripts: {
+        files: ['app/**/*.js'],
+        tasks: ['eslint'],
+        options: {
+          spawn: false
+        },
+      },
+    },
+
+    eslint: {
+      options: {
+        configFile: 'eslint.json'
+      },
+      target: ['app/**/*.js']
+    },
+
+    sass: {
+      dist: {
+        options: {
+          sourcemap: 'none',
+          files: {
+          // destination          // source file
+            "app/css/styles.css" : "app/scss/styles.scss"
+          }
+        }
+      }
+    },
+
+    browserSync: {
+      default_options: {
+        bsFiles: {
+          src: [
+            "app/css/*.css",
+            "*.html",
+            "app/scripts/*.js"
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: "./"
+          }
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.registerTask('default', ['eslint', 'sass', 'browserSync', 'watch']);
+
+};
+
+// broswerSync localhost:3000
